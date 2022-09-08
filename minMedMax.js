3b5119de28ef298881ef70382ead6a285cb083b4
Nawat <nawatsrisaard039@gmail.com>
2022-09-08 14:08:53 +0700

    Update

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,22 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
+  let arr = [n1,n2,n3]
+  let max = n1
+  let mid = n1
+  let min = n1
+
+  for(let number of arr){
+    if(number > max)
+      max = number
+    
+    if(number < min)
+      min = number
+
+    if(number<max && number>min)
+      mid = number
+  }
+
+  return {min,mid,max}
 }
 module.exports = minMedMax
